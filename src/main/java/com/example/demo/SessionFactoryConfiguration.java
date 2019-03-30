927fcd1fe806e983b1cdf19b463fe5a757baad17
rabkool <905956347@qq.com>
2019-03-30 20:01:47 +0800

    加了前端的东西

--- src/main/java/com/example/demo/SessionFactoryConfiguration.java
+++ src/main/java/com/example/demo/SessionFactoryConfiguration.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-
-
 import java.io.IOException;
 
 import javax.sql.DataSource;
@@ -18,40 +16,42 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
 public class SessionFactoryConfiguration {
-	@Value ("${mybatis_config_file}")
+	@Value("${mybatis_config_file}")
 	private String mybatisConfigFilePath;
-	
-	@Value ("${mapper_path}")
+
+	@Value("${mapper_path}")
 	private String maperPath;
-	
-	@Value ("${entity_Package}")
+
+	@Value("${entity_Package}")
 	private String entityPackage;
-	 
+
 	@Autowired
 	@Qualifier("dataSource")
 	private DataSource dataSource;
-	
-	
-	
-	 @Bean(name = "sqlSessionFactory")
-	    public SqlSessionFactoryBean createSqlSessionFactoryBean() throws IOException {
-	        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-	        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource(mybatisConfigFilePath));
-	      
-	        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();  
-	        String packageSearchPath = PathMatchingResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX +maperPath;
-	      
-	        sqlSessionFactoryBean.setMapperLocations(resolver.getResources(packageSearchPath)); 
-	      
-	        sqlSessionFactoryBean.setDataSource(dataSource);
-	      
-	        sqlSessionFactoryBean.setTypeAliasesPackage(entityPackage);	
-	        
-	        return sqlSessionFactoryBean;
-	        
-	        
-	        
 
-	    
-	 }
+	@Bean(name = "sqlSessionFactory")
+	public SqlSessionFactoryBean createSqlSessionFactoryBean() throws IOException {
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		// 设置mybatis configuration 扫描路径
+
+		sqlSessionFactoryBean.setConfigLocation(new ClassPathResource(mybatisConfigFilePath));
+		// 添加mapper 扫描路径
+
+		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+
+		String packageSearchPath = PathMatchingResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + maperPath;
+
+		sqlSessionFactoryBean.setMapperLocations(resolver.getResources(packageSearchPath));
+
+		// 设置dataSource
+
+		sqlSessionFactoryBean.setDataSource(dataSource);
+
+		// 设置typeAlias 包扫描路径
+
+		sqlSessionFactoryBean.setTypeAliasesPackage(entityPackage);
+
+		return sqlSessionFactoryBean;
+
+	}
 }
