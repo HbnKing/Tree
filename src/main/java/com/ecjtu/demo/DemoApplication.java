0e93fa018ebd192cbd5a862230fdde8e36be11c2
rabkool <905956347@qq.com>
2019-03-31 20:06:09 +0800

    q

--- src/main/java/com/ecjtu/demo/DemoApplication.java
+++ src/main/java/com/ecjtu/demo/DemoApplication.java
@@ -1,14 +1,12 @@
-package com.example.demo;
+package com.ecjtu.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
