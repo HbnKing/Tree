b40a25d9ecba455fe84e648f1c6b19ddd13b9a04
rabkool <905956347@qq.com>
2019-04-04 12:13:27 +0800

    4/4

---
+++ src/main/miniprogram-test-3/pages/wxlogin/wxlogin.js
@@ -0,0 +1,42 @@
+const app = getApp()
+
+Page({
+  data: {
+  },
+
+  onLoad: function (params) {
+
+  },
+
+  // 登录  
+  doLogin: function (e) {
+    console.log(e.detail.errMsg)
+    console.log(e.detail.userInfo)
+    console.log(e.detail.rawData)
+
+    wx.login({
+      success: function (res) {
+        console.log(res)
+        // 获取登录的临时凭证
+        var code = res.code;
+        console.log("res.code is"+code);
+
+        // 调用后端，获取微信的session_key, secret
+        wx.request({
+          url: "http://localhost:9011/wxLogin?code=" + code,
+          method: "POST",
+          success: function (result) {
+            console.log(result);
+            // 保存用户信息到本地缓存，可以用作小程序端的拦截器
+            // app.setGlobalUserInfo(e.detail.userInfo);
+            wx.redirectTo({
+              url: '../list/list',
+            })
+          }
+        })
+
+
+      }
+    })
+  }
+})
